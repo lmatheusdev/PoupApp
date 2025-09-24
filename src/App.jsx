47699638feebb5632a58046b2b2d61b3acf3b0d4
lmatheusdev <leandrothferraz@gmail.com>
2025-09-23 23:33:14 -0300

    base do aside

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
-import './App.css'
+import logo from "/logo.svg"
 
 function App() {
   return (
-    <p>Olá mundo</p>
+    <aside>
+      <img src={logo}/>
+      <p>Desenvolvido por Alura. Projeto fictício sem fins comerciais.</p>
+    </aside>
   )
 }
 
